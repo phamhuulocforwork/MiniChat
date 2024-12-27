e63c4aafc4aaf608325255c29a75dc8375ff1dfa
Pham Huu Loc <phamhuulocforwork@gmail.com>
2024-12-27 12:04:30 +0700

    chore: update chat application window size to 1280x720

--- src/main/java/minichat/ChatApplication.java
+++ src/main/java/minichat/ChatApplication.java
@@ -11,7 +11,7 @@ public class ChatApplication extends Application {
   @Override
   public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
       FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("chat-view.fxml"));
-      Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+      Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
       stage.setTitle("Mini Chat");
       stage.setScene(scene);
       stage.show();
